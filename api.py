a7e3da997521f4265c6f4d14ce3080b358288082
Georg <krysopath@gmail.com>
2021-11-21 16:50:49 +0100

    chore: black formatted

--- api.py
+++ api.py
@@ -16,25 +16,21 @@ class increment(Resource):
     def put(self):
         global counter
         time.sleep(2)
-        counter+=1
-        
+        counter += 1
+
         return {"counter": counter}
-    
-    
+
 
 class read(Resource, threading.Thread):
     def get(self):
-        #time.sleep(random.randint(10,40)) 
+        # time.sleep(random.randint(10,40))
         return {"counter": counter}
-       
 
 
-api.add_resource(read, '/counter/read')
-api.add_resource(increment, '/counter/increment')
+api.add_resource(read, "/counter/read")
+api.add_resource(increment, "/counter/increment")
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0', port =5000)
+    app.run(host="0.0.0.0", port=5000)
